1a39624810276cbb1f712d3ff28edc42d75a5d22
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-10-31 16:48:18 -0700

    Change to Halloween theme

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     duedate = (new Date(duedate)).toUTCString().slice(0,-13);
     
     let $item = $('<tr>');
-    let $checkbox = $('<input>').attr('type', 'checkbox');
+    let $checkbox = $('<input>').attr('type', 'checkbox').click(checkOffItem);
     let $xMark = $('<a>').attr('href', '#').text('X').addClass('delete').click(deleteItem);
     $item.append( $('<td>').text(action) )
          .append( $('<td>').text(duedate) )
@@ -24,6 +24,10 @@ $(document).ready(function(){
 
   function deleteItem() {
     $(this).closest('tr').remove();
+  }
+
+  function checkOffItem() {
+    $(this).closest('tr').children().first().toggleClass('done');
   }
 
   function handleKey(e) {
