80a24c5e7ad70fb83ded61986776ce1b08ddd4a9
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-02 14:00:46 -0800

    Implement local storage, but the storage won't yet recognize deletions

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
   let inputForm = $('#action');
   inputForm.focus();
 
+  var items = localStorage.items ? JSON.parse(localStorage.items) : [];
+  items.forEach(function(item) {
+    var $item = createItem(item[0], item[1]);
+    $('#list').append($item);
+  })
+
+  var itemNum = items.length;
+
   $('#add').click(addItem);
   $('#clear').click(clear);
   $(window).on('keypress', handleKey);
@@ -15,16 +23,23 @@ $(document).ready(function(){
       let duedate = $('#duedate').val();
       duedate = (new Date(duedate)).toUTCString().slice(0,-13);
       
-      let $item = $('<tr>').addClass('item');
-      let $checkbox = $('<input>').attr('type', 'checkbox').click(checkOffItem);
-      let $xMark = $('<a>').attr('href', '#').text('X').addClass('delete').click(deleteItem);
-      $item.append( $('<td>').text(action) )
-           .append( $('<td>').text(duedate) )
-           .append( $('<td>').append($checkbox).append($xMark) );
+      let $item = createItem(action, duedate);
+      items.push([action, duedate, false, itemNum]);
+      localStorage.items = JSON.stringify(items);
 
       $('#list').append($item);
       inputForm.val('').focus();
     }
+  }
+
+  function createItem(action, duedate, checked) {
+    let $item = $('<tr>').addClass('item').attr('id', itemNum++);
+    let $checkbox = $('<input>').attr('type', 'checkbox').click(checkOffItem);
+    let $xMark = $('<a>').attr('href', '#').text('X').addClass('delete').click(deleteItem);
+    $item.append( $('<td>').text(action) )
+         .append( $('<td>').text(duedate) )
+         .append( $('<td>').append($checkbox).append($xMark) );
+    return $item;
   }
 
   function deleteItem() {
@@ -33,6 +48,8 @@ $(document).ready(function(){
 
   function clear() {
     $('#list .item').remove();
+    items = [];
+    localStorage.removeItem('items');
     inputForm.focus();
   }
 
